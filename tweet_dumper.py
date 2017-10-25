d6bf91d41b55fa4309fa2ed575a4a49b130740e2
Xianda Gao <gaoxiandathss@gmail.com>
2017-10-25 17:42:13 -0400

    d

--- tweet_dumper.py
+++ tweet_dumper.py
@@ -19,14 +19,11 @@ print(api)
 col = []
 counter = 0
 
-
-
 class MyStreamListener(tweepy.StreamListener):
     
     
     def __init__(self, max_tweets=1000, *args, **kwargs):
         self.max_tweets = max_tweets
-        self.counter = 0
         super().__init__(*args, **kwargs)
     
     def on_connect(self):
@@ -43,26 +40,34 @@ class MyStreamListener(tweepy.StreamListener):
 
     def process(self, data):
         if data['coordinates'] is not None:
+            #print('good')
             tweet_dict = {'text': data['text'],
                          'coordinates': data['coordinates']['coordinates'],
                          'created_at': data['created_at'],
                          'timestamp_ms': data['timestamp_ms'],
                          'user_name': data['user']['name'],
                          'user_screen_name': data['user']['screen_name']}
-            col.append(tweet_dict)
+        else:
+            #print('bad')
+            tweet_dict = {'text': data['text'],
+                         'coordinates': [random.random() * random.choice([180, -180]),
+                                         random.random() * random.choice([90, -90])],
+                         'created_at': data['created_at'],
+                         'timestamp_ms': data['timestamp_ms'],
+                         'user_name': data['user']['name'],
+                         'user_screen_name': data['user']['screen_name']}
+        col.append(tweet_dict)
 
-            counter += 1
-            if counter % 10 == 0:
-                value = int(100.00 * counter / self.max_tweets)
-                mining_time = datetime.now() - self.start_time
-                print("%s/%s" % (counter, self.max_tweets))
-                print("Tweets/Sec: %.1f" % (counter / mining_time.seconds))
-                if counter >= self.max_tweets:
-                    myStream.disconnect()
-                    print("Finished")
-                    print("Total Mining Time: %s" % (mining_time))
-                    print("Tweets/Sec: %.1f" % (self.max_tweets / mining_time.seconds))
-            #self.es.index(index='tweet', doc_type='tweet_data', body=json.loads(json.dumps(tweet_dict)))
+        if len(col) % 10 == 0:
+            value = int(100.00 * len(col) / self.max_tweets)
+            mining_time = datetime.now() - self.start_time
+            print("%s/%s" % (len(col), self.max_tweets))
+            if len(col) >= self.max_tweets:
+                myStream.disconnect()
+                print("Finished")
+                print("Total Mining Time: %d" % (mining_time))
+                print("Tweets/Sec: %.1f" % (self.max_tweets / mining_time.seconds))
+        #self.es.index(index='tweet', doc_type='tweet_data', body=json.loads(json.dumps(tweet_dict)))
 
 
     
@@ -71,43 +76,26 @@ myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
 print(myStream)
 
 
-keywords = ["Jupyter",
-            "Python",
-            "Data Mining",
-            "Machine Learning",
-            "Data Science",
-            "Big Data",
-            "DataMining",
-            "MachineLearning",
-            "DataScience",
-            "BigData",
-            "IoT",
-            "#R",
-            "Trump",
-            "C++",
+keywords = ["C++",
             "Google",
             "Facebook",
             "Columbia",
             "school",
             "university",
-            "America",
-            "American",
-            "chinese",
             "China",
             "music",
-            "hip hop",
-            "R&B",
+            "Machine Learning",
+            "Python",
            ]
 
 # Start a filter with an error counter of 20
 for error_counter in range(20):
     try:
         myStream.filter(track = keywords, locations = [-180,-90,180,90])
-        print("Tweets collected: %s" % myStream.listener.counter)
-        print("Total tweets in collection: %s" % len(col))
+        print("Total tweets in collection: %d" % len(col))
         break
     except:
-        print("ERROR# %s" % (error_counter + 1))
+        print("ERROR# %d" % (error_counter + 1))
 
 open("tweets.txt", "w").write(str(col))
 
--- twittermap/index.py
+++ twittermap/index.py
@@ -1,4 +1,5 @@
-import ast
+# -*- coding: utf-8 -*-
+import ast, json
 from elasticsearch import Elasticsearch
 
 host = 'localhost'
@@ -8,13 +9,20 @@ if __name__=="__main__":
     es = Elasticsearch([{'host': host, 'port': port}])
     #mappings = {"mappings":{"tweet_data":{"properties":{"coordinates":{"type":"geo_point"},"created_at":{"type":"text","fields":{"keyword":{"type":"keyword","ignore_above":256}}},"text":{"type":"text","fields":{"keyword":{"type":"keyword","ignore_above":256}}},"timestamp_ms":{"type":"text","fields":{"keyword":{"type":"keyword","ignore_above":256}}},"user_name":{"type":"text","fields":{"keyword":{"type":"keyword","ignore_above":256}}},"user_screen_name":{"type":"text","fields":{"keyword":{"type":"keyword","ignore_above":256}}}}}},"settings":{"index":{"number_of_shards":"5","number_of_replicas":"1"}}}
     mappings = {"mappings":{"tweet_data":{"properties":{"coordinates":{"type":"geo_point"},"created_at":{"type":"text"},"text":{"type":"text"},"timestamp_ms":{"type":"text"},"user_name":{"type":"text"},"user_screen_name":{"type":"text"}}}}}
-    es.indices.create(index='tweet', body=mappings)
-    tweets = []
-    with open("test.txt") as f:
-        lines = f.readlines()
-        for line in lines:
-            line = line[0:-1]
-            content = ast.literal_eval(line)
-            es.index(index='tweet', doc_type='tweet_data', body=content)
-            tweets.append(content)
+    try:
+        es.indices.create(index='tweet', body=mappings)
+    except:
+        pass
 
+    error = 0
+    with open("tweets_coord.txt", "r") as f:
+        tweets = eval(f.read())
+        print len(tweets)
+        for item in tweets:
+            #print(item)
+            try:
+                es.index(index='tweet', doc_type='tweet_data', body=item)
+            except:
+                error += 1
+            #tweets.append(item)
+    print error
